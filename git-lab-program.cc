c1f345653e5f5a988fa3c3a558e8712b47e64433
Nathan <nf208619@ohio.edu>
2020-09-04 15:44:32 -0400

    Initial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,20 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Nathan Fout
+ *        Date: 09/04/2020
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+
+    /*add code*/
+    return 0;
+}// main
